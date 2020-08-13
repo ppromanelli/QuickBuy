bfa432cb0ab89820efdf602f4344f4b0996b2590
Pedro <ppromanelli@gmail.com>
2020-08-13 11:00:00 -0300

    Entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+         public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+        /// <summary>
+        /// Usuario pode ter 0 ou n pedidos
+        /// </summary>
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
